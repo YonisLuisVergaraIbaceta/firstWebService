7f965f43aa2cb6cbf1984d66fe0e3e377e942463
usuario <usuario@10.15.72.61>
2018-06-27 23:45:26 -0400

    Paths changes

--- adm_user (1).sql
+++ adm_user (1).sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.0
+-- version 4.8.0.1
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 24-05-2018 a las 06:17:50
--- Versión del servidor: 10.1.31-MariaDB
--- Versión de PHP: 7.2.4
+-- Tiempo de generación: 28-06-2018 a las 05:42:24
+-- Versión del servidor: 10.1.32-MariaDB
+-- Versión de PHP: 7.2.5
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -36,16 +36,17 @@ CREATE TABLE `usuario` (
   `name` varchar(100) DEFAULT NULL,
   `birthdate` date NOT NULL,
   `id_nacional` varchar(100) DEFAULT NULL,
-  `estado` varchar(1) DEFAULT NULL
+  `estado` varchar(1) DEFAULT NULL,
+  `departamento` varchar(60) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `usuario`
 --
 
-INSERT INTO `usuario` (`id_usuario`, `email`, `passwer`, `last_name`, `name`, `birthdate`, `id_nacional`, `estado`) VALUES
-(1, 'vergara.yonis@gmail.com', 'yoni', 'Vergara Ibaceta ', 'Yonis Luis', '1988-11-17', '17118537', 'A'),
-(2, 'kiara.vergara@gmail.com', '1234', 'vergara cobarubia', 'Kiara Araseli', '2014-10-16', '1231412', 'D');
+INSERT INTO `usuario` (`id_usuario`, `email`, `passwer`, `last_name`, `name`, `birthdate`, `id_nacional`, `estado`, `departamento`) VALUES
+(1, 'vergara.yonis@gmail.com', 'yoni', 'Vergara Ibaceta ', 'Yonis Luis', '1988-11-17', '17118537', 'A', 'ADM'),
+(2, 'kiara.vergara@gmail.com', '1234', 'vergara cobarubia', 'Kiara Araseli', '2014-10-16', '1231412', 'D', 'RRHH');
 
 --
 -- Índices para tablas volcadas
